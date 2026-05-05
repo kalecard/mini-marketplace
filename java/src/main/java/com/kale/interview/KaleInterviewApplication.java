68b70db273255054935ae855f399e4aabbdd4ee0
Luis Molina <luis@kalecard.com>
2026-05-05 10:38:50 -0400

    Add Java module skeleton (Spring Boot 3.3.4, JDK 17, Maven Wrapper)

---
+++ java/src/main/java/com/kale/interview/KaleInterviewApplication.java
@@ -0,0 +1,12 @@
+package com.kale.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaleInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaleInterviewApplication.class, args);
+    }
+}
